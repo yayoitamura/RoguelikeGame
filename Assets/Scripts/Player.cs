1cf5192371ff0c16e41350fa84c77760262440f5
yayoitamura <gh3520900@gmail.com>
2017-12-26 20:44:56 +0900

    Add player's move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,45 +4,62 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 	Animator animator;
+	Vector2 position;
+	public Vector2 SPEED = new Vector2 (0.05f, 0.05f);
 
 	public enum InputKey {
 		Left, // 左
 		Up, // 上
 		Right, // 右
 		Down, // 下
+
+		Empty,
 		};
-		public InputKey key = InputKey.Down;
+		public InputKey key = InputKey.Empty;
 
 		void Start () {
 		animator = GetComponent<Animator> ();
+		position = transform.position;
 	}
 
 	void Update () {
-		if (Input.GetKey (KeyCode.LeftArrow)) {
+
+		if (Input.GetKey ("left")) {
 			key = InputKey.Left;
-		}
-		if (Input.GetKey (KeyCode.UpArrow)) {
+
+		} else if (Input.GetKey ("up")) {
 			key = InputKey.Up;
-		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
+
+		} else if (Input.GetKey ("right")) {
 			key = InputKey.Right;
-		}
-		if (Input.GetKey (KeyCode.DownArrow)) {
+
+		} else if (Input.GetKey ("down")) {
 			key = InputKey.Down;
 		}
+		transform.position = position;
+
+		// Debug.Log ("key  ======  " + key);
 
 		switch (key) {
 			case InputKey.Left:
-				animator.SetTrigger ("left");
+				// animator.SetTrigger ("left");
+				Debug.Log ("state  left");
+				position.x -= SPEED.x;
 				break;
 			case InputKey.Up:
-				animator.SetTrigger ("up");
+				// animator.SetTrigger ("up");
+				Debug.Log ("state  up");
+				position.y += SPEED.y;
 				break;
 			case InputKey.Right:
-				animator.SetTrigger ("right");
+				// animator.SetTrigger ("right");
+				Debug.Log ("state  right");
+				position.x += SPEED.x;
 				break;
 			case InputKey.Down:
-				animator.SetTrigger ("down");
+				// animator.SetTrigger ("down");
+				Debug.Log ("state  down");
+				position.y -= SPEED.y;
 				break;
 		}
 
