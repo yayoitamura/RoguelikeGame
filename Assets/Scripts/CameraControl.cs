62a4e4b8ee048b2758b8c6304b8816f8651fd3dc
yayoitamura <gh3520900@gmail.com>
2018-01-05 19:42:34 +0900

    fix CameraControl

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 namespace Completed {
 
-	public class CameraControl : MonoBehaviour {
-
+	public class CameraControl : MonoBehaviour { //-1 -2.5,4 5.5
+		const float MOVE_MAX_X = 4.0f;
+		const float MOVE_MAX_Y = 5.5f;
+		const float MOVE_MIN_X = -1.0f;
+		const float MOVE_MIN_Y = -2.5f;
 		public GameObject player;
-		public int y = 5;
-		public int z = -10;
 
 		void Start () {
 
 		}
 
 		void Update () {
-			transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, z);
+			float cameraPositionX = Mathf.Clamp (player.transform.position.x, MOVE_MIN_X, MOVE_MAX_X);
+			float cameraPositionY = Mathf.Clamp (player.transform.position.y, MOVE_MIN_Y, MOVE_MAX_Y);
+			float z = -10f;
+			transform.position = new Vector3 (cameraPositionX, cameraPositionY, z);
 		}
 
 	}
