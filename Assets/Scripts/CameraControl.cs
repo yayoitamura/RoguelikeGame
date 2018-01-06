ad89edef73d3656d3494bba06d5cff070783cf94
yayoitamura <gh3520900@gmail.com>
2018-01-06 16:46:19 +0900

    Add DOTween

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,25 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Completed {
+public class CameraControl : MonoBehaviour { //-1 -2.5,4 5.5
+	const float MOVE_MAX_X = 4.0f;
+	const float MOVE_MAX_Y = 5.5f;
+	const float MOVE_MIN_X = -1.0f;
+	const float MOVE_MIN_Y = -2.5f;
+	public GameObject player;
 
-	public class CameraControl : MonoBehaviour { //-1 -2.5,4 5.5
-		const float MOVE_MAX_X = 4.0f;
-		const float MOVE_MAX_Y = 5.5f;
-		const float MOVE_MIN_X = -1.0f;
-		const float MOVE_MIN_Y = -2.5f;
-		public GameObject player;
-
-		void Start () {
-
-		}
-
-		void Update () {
-			float cameraPositionX = Mathf.Clamp (player.transform.position.x, MOVE_MIN_X, MOVE_MAX_X);
-			float cameraPositionY = Mathf.Clamp (player.transform.position.y, MOVE_MIN_Y, MOVE_MAX_Y);
-			float z = -10f;
-			transform.position = new Vector3 (cameraPositionX, cameraPositionY, z);
-		}
+	void Start () {
 
 	}
+
+	void Update () {
+
+	}
+
+	public void Move () {
+		float cameraPositionX = Mathf.Clamp (player.transform.position.x, MOVE_MIN_X, MOVE_MAX_X);
+		float cameraPositionY = Mathf.Clamp (player.transform.position.y, MOVE_MIN_Y, MOVE_MAX_Y);
+		float z = -10f;
+		transform.position = new Vector3 (cameraPositionX, cameraPositionY, z);
+	}
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	Vector2 currentPosition;
-	private const int STEP = 1;
-	private const float HEIGH = 6.5f;
-	private const float WIDTH = 5.5f;
 	private float speed = 5.0f;
 	Vector2 mousePosition;
+	public DungeonGenerator generator;
+	[SerializeField]
+	GameObject wallPrefab;
+	[SerializeField]
+	GameObject floorPrefab;
+	[SerializeField]
+	Transform tileContainer;
 
-	void Start () { }
+	CameraControl cameraControl;
+
+	void Start () {
+
+		cameraControl = GameObject.Find ("Main Camera").GetComponent<CameraControl> ();
+
+		var map = generator.Generate ();
+
+		// マップを元にオブジェクト生成
+		for (var x = 0; x < generator.width; x++) {
+			for (var y = 0; y < generator.height; y++) {
+				Debug.Log ("-----------");
+				var tile = map[x, y] == 1 ? Instantiate (floorPrefab) : Instantiate (wallPrefab);
+				tile.transform.SetParent (tileContainer);
+				tile.transform.localPosition = new Vector2 (x, y);
+			}
+		}
+	}
 
 	void Update () {
+		Move ();
+	}
+	
 
+	void Move () {
 		if (Input.GetMouseButton (0)) {
 			mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			float step = speed * Time.deltaTime;
+			cameraControl.Move ();
 
 			Ray ray = new Ray (mousePosition, transform.forward);
 			RaycastHit2D hit = Physics2D.Raycast ((Vector2) ray.origin, (Vector2) ray.direction, 10);
+
 			if (hit.collider) {
 				mousePosition = transform.position;
 				CantMove (hit);
+
 			}
 
 			transform.position = Vector2.MoveTowards (transform.position, new Vector2 (mousePosition.x, mousePosition.y), step);
 		}
-
 	}
 
 	void CantMove (RaycastHit2D hit) {
