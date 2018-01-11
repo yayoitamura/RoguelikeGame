d32ec11ac77f92d8346f394472b967c2639ffd55
yayoitamura <gh3520900@gmail.com>
2018-01-11 20:42:51 +0900

    fix player's move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,24 @@ public class Player : MonoBehaviour {
 	const float STEP = 0.5f;
 	Vector2 movePosition;
 
+	// bool push;
+	string key;
+
+	Vector3 MOVEX = new Vector3 (0.5f, 0, 0);
+	Vector3 MOVEY = new Vector3 (0, 0.5f, 0);
+
+	//長押しと判定する時間、メソッドを実行する間隔
+	// [SerializeField]
+	private float _longPressTime = 0.3f, _invokeInterval = 0.05f;
+
+	//長押しと判定するまで or 次のメソッドを実行するまでの時間
+	private float _waitTime = 0;
+
+	//押しているか
+	private bool _isPressing = false;
+
+	//一度でもメソッドを実行したか
+	private bool _isInvokedEvent = false;
 	void Start () {
 
 		movePosition = transform.position;
@@ -22,52 +40,89 @@ public class Player : MonoBehaviour {
 		animator = GetComponent<Animator> ();
 	}
 
-	public void SetTargetPosition (string key) {
+	void Update () {
 
-		switch (key) {
-			case "left":
-				movePosition.x -= STEP;
-				Debug.Log ("state  left");
-				break;
-			case "up":
-				movePosition.y += STEP;
-				Debug.Log ("state  up");
-				break;
-			case "right":
-				movePosition.x += STEP;
-				Debug.Log ("state  right");
-				break;
-			case "down":
-				movePosition.y -= STEP;
-				Debug.Log ("state  down");
-				break;
+		// if (push == true) {
+		// 	SetTargetPosition ();
+		// }
+		//ボタンが押されていない時はスルー
+		if (!_isPressing) {
+			return;
+		}
+		//待ち時間を減らす
+		_waitTime -= Time.deltaTime;
+		//待ち時間がまだある時はスルー
+		if (_waitTime > 0) {
+			return;
 		}
 
-		SetAnimationParam (key);
+		//メソッド実行、待ち時間設定
+		// _event.Invoke ();
+		SetTargetPosition ();
+		_waitTime = _invokeInterval;
+		_isInvokedEvent = true;
+
+	}
+
+	public void PushDown (string pushButton) {
+
+		// push = true;
+		key = pushButton;
+		// Debug.Log ("push   ---  " + key);
+		_isPressing = true;
+		_isInvokedEvent = false;
+		_waitTime = _longPressTime;
+
+	}
+
+	public void PushUp () {
+
+		// push = false;
+		_isPressing = false;
+
+	}
+
+	public void Click () {
+		Debug.Log ("click");
+		//一度もイベントが実行されていなければ実行
+		if (!_isInvokedEvent) {
+			Debug.Log ("1click");
+			// _event.Invoke ();
+			SetTargetPosition ();
+		}
+	}
+
+	public void SetTargetPosition () {
+
+		// if (push == true) {
+		switch (key) {
+
+			case "left":
+				movePosition = transform.position - MOVEX; //.x -= STEP; // * Time.deltaTime;
+				break;
+
+			case "up":
+				movePosition = transform.position + MOVEY; //.y += STEP;
+				break;
+
+			case "right":
+				movePosition = transform.position + MOVEX; //.x += STEP;
+				break;
+
+			case "down":
+				movePosition = transform.position - MOVEY; //.y -= STEP;
+				break;
+		}
+		// }
+
+		SetAnimationParam ();
 		Move ();
 	}
 
-	void SetAnimationParam (string key) {
+	void SetAnimationParam () {
 
-		if (key == "left") {
-			animator.Play ("chaLeft");
-			return;
-		}
+		animator.Play (key);
 
-		if (key == "right") {
-			animator.Play ("chaRight");
-			return;
-		}
-
-		if (key == "down") {
-			animator.Play ("chaDown");
-			return;
-		}
-
-		if (key == "up") {
-			animator.Play ("chaUp");
-			return;
-		}
 	}
 
 	void Move () {
@@ -81,15 +136,19 @@ public class Player : MonoBehaviour {
 			movePosition = transform.position;
 			CantMove (hit);
 		} else {
-			transform.position = movePosition;
+			float step = 5f;
+			// transform.position = movePosition;
+			transform.position = Vector3.MoveTowards (transform.position, movePosition, step * Time.deltaTime);
 		}
 	}
 
 	void CantMove (RaycastHit2D hit) {
 
 		if (hit.collider.gameObject.tag == "enemy") { }
-
-		if (hit.collider.gameObject.tag == "wall") { }
+		// Debug.Log ("enemy");
+		if (hit.collider.gameObject.tag == "wall") {
+			// Debug.Log ("ray");
+		}
 
 		if (hit.collider.gameObject.tag == "steps") {
 			int nextStage = 0;
@@ -99,9 +158,12 @@ public class Player : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "wall") {
+			Debug.Log ("colision");
 
-		Debug.Log ("On Collision == " + other.gameObject.tag);
-		movePosition = transform.position;
+		} else {
+			movePosition = transform.position;
+		}
 	}
 
 }
