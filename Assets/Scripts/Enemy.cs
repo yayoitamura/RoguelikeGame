ddd3913a2436cbc3f8fb88871e478fbd74597732
yayoitamura <gh3520900@gmail.com>
2018-01-25 19:16:28 +0900

    fix

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+  bool enemyTurn;
 
   GameObject man;
 
@@ -10,4 +11,20 @@ public class Enemy : MonoBehaviour {
     man = GameObject.Find ("Man");
   }
 
+  public void ChasePlayer (GameObject target) {
+    Debug.Log ("move enemy");
+    Vector2　 targetPsition = target.transform.position;
+
+    float x = targetPsition.x;
+    float y = targetPsition.y;
+    //追跡方向の決定
+    Vector2 direction = new Vector2 (x - transform.position.x, y - transform.position.y).normalized;
+    //ターゲット方向に力を加える
+    GetComponent<Rigidbody2D> ().velocity = (direction * 2);
+
+  }
+
+  public void dontChase () {
+    GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+  }
 }
--- Assets/Scripts/SearchCharacter.cs
+++ Assets/Scripts/SearchCharacter.cs
@@ -5,34 +5,23 @@ public class SearchCharacter : MonoBehaviour {
 	GameObject target;
 	GameObject enemy;
 
+	Vector2 targetPsition;
+
 	void Start () {
 		target = GameObject.Find ("Man");
-		enemy = GameObject.Find ("Enemy");
+		enemy = transform.parent.gameObject;
 	}
 
-	// void OnTriggerStay2D (Collider2D other) {
-	// 	if (other.tag == "player") {
-	// 		Vector2 targetPosition = target.transform.position;
-	// 		Debug.Log (targetPosition);
-	// 		enemy.transform.position = targetPosition;
-
-	// 	}
-	// }
-
-	private void Update () {
-		Move ();
+	void OnTriggerStay2D (Collider2D other) {
+		if (other.tag == "player") {
+			enemy.GetComponent<Enemy> ().ChasePlayer (other.gameObject); 
+		}
 	}
 
-	void Move () {
-		Vector2 Predetor = target.transform.position;
-
-		float x = Predetor.x;
-		float y = Predetor.y;
-		//追跡方向の決定
-		Vector2 direction = new Vector2 (x - enemy.transform.position.x, y - enemy.transform.position.y).normalized;
-		//ターゲット方向に力を加える
-		enemy.GetComponent<Rigidbody2D> ().velocity = (direction * 4);
-
+	private void OnTriggerExit2D (Collider2D other) {
+		if (other.tag == "player") {
+			enemy.GetComponent<Enemy> ().dontChase ();
+		}
 	}
 
 }
