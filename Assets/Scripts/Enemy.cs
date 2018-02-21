280cee3fbdf266e3137b0b3f121af01bc90a5ffd
yayoitamura <gh3520900@gmail.com>
2018-02-21 16:14:18 +0900

    refactor

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,6 +13,45 @@ public class Enemy : MonoBehaviour {
     player = GameObject.Find ("Man").GetComponent<Player> ();
   }
 
+  //PLAYINGstateの時だけにキャラクターを動かせるようにしたい
+  //pkayingで呼び出す→死んだ」タイミングでステート切り替え
+  // 
+
+  // void Update () {
+
+	// 	switch (GameState.instance.state) {
+	// 		case GameState.Game.READY:
+	// 			GameState.instance.state = GameState.Game.START;
+	// 			break;
+	// 		case GameState.Game.START:
+	// 			//スタート
+	// 			//ボタン操作○ プレイヤ× 敵×
+	// 			//メニュー画面
+	// 			break;
+	// 		case GameState.Game.PREPARE:
+	// 			//準備
+	// 			//ボタン操作× プレイヤ× 敵×
+	// 			//シーン以降・フェード・ダンジョン生成
+	// 			Prepare ();
+	// 			GameObject.Find ("Man").GetComponent<Player> ().PlayerSetUp ();
+	// 			GameState.instance.state = GameState.Game.PLAYING;
+	// 			break;
+	// 		case GameState.Game.PLAYING:
+
+	// 			GameObject.Find ("Main Camera").GetComponent<CameraControl> ().PlayerLookOn ();
+	// 			//プレイ中
+	// 			//ボタン操作○ プレイヤ○ 敵○
+	// 			//プレイ開始→プレイ終了（死or階段）
+	// 			break;
+	// 		case GameState.Game.END:
+	// 			//ゲーム終了
+	// 			//ボタン操作○ プレイヤ× 敵×
+	// 			//ゲームオーバー/クリア画面
+	// 			break;
+	// 	}
+	// }
+
+
   void Update () {
     if (!player.playerTurn) {
       attemptMove ();
